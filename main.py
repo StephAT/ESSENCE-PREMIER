c324441cfef80044595359331de12c085c8473d1
Stephen Addo <stephenaddo201@gmail.com>
2026-04-29 20:17:42 +0000

    Fix CORS middleware origins formatting

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ app = FastAPI(
 # ── CORS ──────────────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://essencepremier.netlify.app/"],
-    "http://localhost:5173",
+    allow_origins=[
+        "https://essencepremier.netlify.app",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
